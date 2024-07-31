ff0707af081a1fcb2c202e7a65b7c1e61a706bc7
ggeorgiev-influx <97943882+ggeorgiev-influx@users.noreply.github.com>
2024-07-31 09:28:08 +0300

    Update ublox-up-down-pin.sh

--- recipes-influx/influx-files/influx-files/opt/influx/ublox-up-down-pin.sh
+++ recipes-influx/influx-files/influx-files/opt/influx/ublox-up-down-pin.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-echo 0 > /sys/class/gpio/gpio9/value
-echo 0 > /sys/class/gpio/gpio6/value
+#echo 0 > /sys/class/gpio/gpio9/value
+#echo 0 > /sys/class/gpio/gpio6/value
+#sleep 1
+#echo 1 > /sys/class/gpio/gpio9/value	
+#echo 1 > /sys/class/gpio/gpio6/value
+
+echo 0 > /sys/class/leds/ublox_VCC/brightness           # JB02 - VCC		reverce logic
+echo 0 > /sys/class/leds/ublox_PWR_ON/brightness        # JB14 - RWR_ON
+echo 0 > /sys/class/leds/ublox_RESET/brightness         # JB08 - RESET
 sleep 1
-echo 1 > /sys/class/gpio/gpio6/value
-sleep 1
-echo 1 > /sys/class/gpio/gpio9/value
+echo 1 > /sys/class/leds/ublox_VCC/brightness
+echo 1 > /sys/class/leds/ublox_PWR_ON/brightness
+echo 1 > /sys/class/leds/ublox_RESET/brightness
+
