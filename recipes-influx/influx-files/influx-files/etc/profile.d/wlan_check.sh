0f41e6a4ed736c52f27fca083e9dae8f59e67893
ggeorgiev-influx <97943882+ggeorgiev-influx@users.noreply.github.com>
2024-09-13 13:51:05 +0300

    Update wlan_check.sh

--- recipes-influx/influx-files/influx-files/etc/profile.d/wlan_check.sh
+++ recipes-influx/influx-files/influx-files/etc/profile.d/wlan_check.sh
@@ -6,7 +6,7 @@ if [ "$wlan0_active" == "inactive" ];
 then
     echo -e "\e[32;1mInfo:\e[0m \e[32m To enable wlan support service, start manually these commands. \e[0m"
     echo -e "\e[32m		switch_module.sh 1MW \e[0m"
-    echo -e "\e[32m		fw_setenv fdt_file imx8mm-influx-redge-1mw.dtb \e[0m"
+    echo -e "\e[32m		fw_setenv fdt_file imx8mm-influx-rex-smart_v2-1mw.dtb \e[0m"
     echo -e "\e[32m		reboot \e[0m"
     echo ""	
 fi
