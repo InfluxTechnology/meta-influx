29a08fc152fab9ad9004aa4e71525efa52916b5a
ggeorgiev-influx <97943882+ggeorgiev-influx@users.noreply.github.com>
2024-09-17 15:43:33 +0300

    Update driver_reconnect.sh

--- recipes-influx/influx-files/influx-files/opt/influx/driver_reconnect.sh
+++ recipes-influx/influx-files/influx-files/opt/influx/driver_reconnect.sh
@@ -4,7 +4,6 @@
 #depmod -a
 modprobe rexgen_usb
 
-dmesg -C
 #power off/on USB to load rexgen_usb driver
 /usr/sbin/uhubctl -a off -p 1 -l 1
 /usr/sbin/uhubctl -a on -p 1 -l 1
@@ -12,5 +11,4 @@ dmesg -C
 ip link set can0 type can bitrate 500000
 ip link set up can0
 
-dmesg | grep RexGen
 
