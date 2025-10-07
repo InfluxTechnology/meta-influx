9ac71d05d1fd470f5a06e2bb6a38316a1f689625
ggeorgiev-influx <97943882+ggeorgiev-influx@users.noreply.github.com>
2025-10-07 13:33:14 +0300

    Update autostart.sh

--- recipes-influx/influx-files/influx-files/opt/influx/autostart.sh
+++ recipes-influx/influx-files/influx-files/opt/influx/autostart.sh
@@ -4,16 +4,6 @@ export EDITOR=/usr/bin/nano	# crontab uses this editor
 # execute at first time
 /etc/profile.d/enable_services.sh
 
-# Mender image update need this 
-# remove/add this crontab jobs
-/usr/bin/crontab -u root -l |  grep -v "* /opt/influx/release_check.sh" |  /usr/bin/crontab -
-#
-# extract preserved files
-if test -f /data/mender/preserved-files.tgz; then
-    tar -xzf /data/mender/preserved-files.tgz -C /
-    mv /data/mender/preserved-files.tgz /data/mender/preserved-files_old.tgz 
-fi
-
 # Power up the Quectel chip
 /opt/influx/cellular_module_start.sh
 
