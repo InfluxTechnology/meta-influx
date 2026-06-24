361c25304882f6d59c597efb618c12908ae530f0
ggeorgiev-influx <97943882+ggeorgiev-influx@users.noreply.github.com>
2026-06-24 13:04:18 +0300

    Update autostart.sh

--- recipes-influx/influx-files/influx-files/opt/influx/autostart.sh
+++ recipes-influx/influx-files/influx-files/opt/influx/autostart.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+# Mender image update need this
+# remove/add this crontab jobs
+/usr/bin/crontab -u root -l |  grep -v "* /opt/influx/release_check.sh" |  /usr/bin/crontab -
+#
+# extract preserved files
+if test -f /data/mender/preserved-files.tgz; then
+    tar -xzf /data/mender/preserved-files.tgz -C /
+    mv /data/mender/preserved-files.tgz /data/mender/preserved-files_old.tgz
+fi
+#
+# Avoiding the sleep during OTA update
+INF_UPG=$(/usr/bin/fw_printenv influx_upgrade | cut -d = -f 2)
+if [ "$INF_UPG" == "1" ]; then
+        /opt/influx/mender_update_finish.sh
+fi
+
 # Power up the Quectel chip
 /opt/influx/cellular_module_start.sh
 
@@ -7,8 +23,7 @@
 #/opt/influx/lte_start_wvdial.sh
 #/opt/influx/start_ppp0.sh
 
-echo 0 > /sys/block/mmcblk2boot0/force_ro
-
 systemctl stop serial-getty@ttymxc1.service
 systemctl disable serial-getty@ttymxc1.service
 
+/opt/influx/local_settings.sh
