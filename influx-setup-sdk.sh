dd93aabff0dc766669dd27dbe5665fa7be1067eb
ggeorgiev-influx <97943882+ggeorgiev-influx@users.noreply.github.com>
2026-07-07 09:02:54 +0300

    Fix commented out copy command in influx-setup-sdk.sh

--- influx-setup-sdk.sh
+++ influx-setup-sdk.sh
@@ -54,7 +54,7 @@ clean_up()
     exit_message clean_up
 }
 
-#cp .repo/manifests/Release-notes sources/meta-influx/recipes-influx/influx-files/influx-files/opt/influx/
+cp .repo/manifests/Release-notes sources/meta-influx/recipes-influx/influx-files/influx-files/opt/influx/
 rm sources/meta-imx/meta-imx-bsp/recipes-graphics/xwayland/xwayland_23.1.1.imx.bbappend
 rm -fr sources/meta-imx/meta-imx-bsp/recipes-kernel/cryptodev/
 
